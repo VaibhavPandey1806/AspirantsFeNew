ce4798ed4f531e5f489f80aed74374076ecef31b
Vaibhav <vaibhav.ppp78@gmail.com>
2024-12-04 02:07:06 +0530

    OK

--- src/utils/comstants.ts
+++ src/utils/comstants.ts
@@ -1,4 +1,6 @@
-const LOGINURL='aspirantsclub-production.up.railway.app/login'
-const LOGOUTURL='aspirantsclub-production.up.railway.app/logout'
+const LOGINURL='https://aspirantsclub-production.up.railway.app/login'
+const LOGOUTURL='https://aspirantsclub-production.up.railway.app/logout'
 
+// const LOGINURL='http://localhost:8086/login'
+// const LOGOUTURL='http://localhost:8086/logout'
 export {LOGINURL,LOGOUTURL};
