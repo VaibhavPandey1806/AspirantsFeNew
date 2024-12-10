c2f255d953e965c9753e36328d211f6ec9420b64
VaibhavPandey1806 <88294124+VaibhavPandey1806@users.noreply.github.com>
2024-12-11 03:14:36 +0530

    Update comstants.ts

--- src/utils/comstants.ts
+++ src/utils/comstants.ts
@@ -1,8 +1,8 @@
 // const LOGINURL='https://aspirantsclub-production.up.railway.app/login'
 // const LOGOUTURL='https://aspirantsclub-production.up.railway.app/logout'
 
-const LOGINURL='https://35.158.118.57:8086/login'
-const LOGOUTURL='https://35.158.118.57:8086/logout'
+const LOGINURL='http://35.158.118.57:8086/login'
+const LOGOUTURL='http://35.158.118.57:8086/logout'
 
 
 // const LOGINURL='http://localhost:8086/login'
