f24e2b64489b07b259d40abe682b8ed0ac9b30b8
VaibhavPandey1806 <88294124+VaibhavPandey1806@users.noreply.github.com>
2024-12-11 03:14:06 +0530

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,6 +1,6 @@
 // export const API_BASE_URL = 'https://aspirantsclub-production.up.railway.app';
 
-export const API_BASE_URL = 'https://35.158.118.57:8086';
+export const API_BASE_URL = 'http://35.158.118.57:8086';
 
 // // Add other API-related configuration here if needed
 export const API_TIMEOUT = 30000; // 30 seconds
